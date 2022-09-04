0bdbfff58d7a4e422bcaad9427a32531c55649d5
NarekGames <112538659+NarekGames@users.noreply.github.com>
2022-09-04 20:08:14 +0400

    Add files via upload

---
+++ Narek Games/js/Index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    document.getElementById("space").scrollIntoView();
+}
+
+function ScrollToNews() {
+    document.getElementById("news1").scrollIntoView();
+}
+
+function ScrollToHelp() {
+    document.getElementById("faceboook").scrollIntoView();
+}
+
+function Download() {
+    window.open('Google Helicopter V2.0.apk');
+}
+
+function DropDown() {
+    document.getElementById("dropdown").classList.toggle("show");
+  }
